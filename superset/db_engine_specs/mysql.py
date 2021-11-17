25954a0113355ccb68c3848d9694e40a17f33f8c
DengYun <dengyun@meideng.net>
2021-11-17 14:03:20 +0800

    modify default week spec for mysql

--- superset/db_engine_specs/mysql.py
+++ superset/db_engine_specs/mysql.py
@@ -107,14 +107,17 @@ class MySQLEngineSpec(BaseEngineSpec, BasicParametersMixin):
         "INTERVAL (HOUR({col})*60 + MINUTE({col})) MINUTE)",
         "PT1H": "DATE_ADD(DATE({col}), " "INTERVAL HOUR({col}) HOUR)",
         "P1D": "DATE({col})",
-        "P1W": "DATE(DATE_SUB({col}, " "INTERVAL DAYOFWEEK({col}) - 1 DAY))",
+        "P1W": "DATE(DATE_SUB({col}, "
+            "INTERVAL DAYOFWEEK(DATE_SUB({col}, "
+            "INTERVAL 1 DAY)) - 1 DAY))",
         "P1M": "DATE(DATE_SUB({col}, " "INTERVAL DAYOFMONTH({col}) - 1 DAY))",
         "P0.25Y": "MAKEDATE(YEAR({col}), 1) "
         "+ INTERVAL QUARTER({col}) QUARTER - INTERVAL 1 QUARTER",
         "P1Y": "DATE(DATE_SUB({col}, " "INTERVAL DAYOFYEAR({col}) - 1 DAY))",
-        "1969-12-29T00:00:00Z/P1W": "DATE(DATE_SUB({col}, "
-        "INTERVAL DAYOFWEEK(DATE_SUB({col}, "
-        "INTERVAL 1 DAY)) - 1 DAY))",
+        # "1969-12-29T00:00:00Z/P1W": "DATE(DATE_SUB({col}, "
+        #     "INTERVAL DAYOFWEEK(DATE_SUB({col}, "
+        #     "INTERVAL 1 DAY)) - 1 DAY))",
+        "1969-12-28T00:00:00Z/P1W": "DATE(DATE_SUB({col}, " "INTERVAL DAYOFWEEK({col}) - 1 DAY))",
     }
 
     type_code_map: Dict[int, str] = {}  # loaded from get_datatype only if needed
